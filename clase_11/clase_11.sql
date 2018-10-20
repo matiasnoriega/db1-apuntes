8794623d86807d897a42c625b8ad95ebef606a7b
Matias Nicolas Noriega <matiasnoriega86@gmail.com>
2018-10-19 23:30:00 -0300

    final clase 11

--- clase_11/clase_11.sql
+++ clase_11/clase_11.sql
@@ -25,7 +25,7 @@ SELECT c.nrocli, c.nyape, count(f.nrofactura)
                             WHERE EXISTS(SELECT 1 FROM detalles d WHERE  a.rubro = 3)
                             AND f.fecha BETWEEN '01-03-2010' AND '30-04-2010'
                                 GROUP BY c.nrocli
-
+-- MAL Porque aumenta la cardinalidad de facturas, porque no correlaciono con los detalles, hay más detalles que facturas y me los cuenta a todos.
 SELECT c.nrocli, c.nyape, count(f.nrofactura)
                     FROM clientes c INNER JOIN facturas f ON c.nrocli = f.cliente
                         INNER JOIN detalles d ON f.nrofactura = d.nrofactura
@@ -59,3 +59,41 @@ SELECT f.cliente, count(*), nyape
                 GROUP BY f.cliente, nyape
                     HAVING count(*) > 2
                     AND count(*) < 5
+
+----------------------
+Division en Alumnos (DBFacu)
+
+error comun en la division ---> METER ATRIBUTOS DE MAS
+
+select id_carr, localidad from alumnos l
+	WHERE id_carr IS NOT NULL AND NOT EXISTS(SELECT id_carr FROM alumnos c 
+					 		WHERE id_carr IS NOT NULL AND NOT EXISTS(select localidad, id_carr FROM alumnos x
+											WHERE x.localidad = l.localidad
+											AND x.id_carr = c.id_carr))
+
+-- PRACTICA 10/EJERCICIO T/a
+-- T) a) Listar los clientes compraron TODOS los articulos (1 fila - cliente 109)
+
+SELECT c.* FROM clientes c 
+        WHERE NOT EXISTS(SELECT nroartic FROM articulos a WHERE NOT EXISTS(
+            SELECT cliente, articulo 
+                FROM detalles d INNER JOIN facturas f ON d.nrofactura = f.nrofactura
+                    WHERE f.cliente = c.nrocli
+                    AND d.articulo = a.nroartic
+        ))
+
+-- RESPUESTA DISTINTA, MAS COMPLICADA
+
+SELECT * 
+    FROM clientes c
+        WHERE (SELECT COUNT(*) FROM articulos)
+        =
+        (SELECT COUNT(*) FROM articulos
+            WHERE nroartic IN(SELECT fd.articulo
+                FROM facturas fc, detalles fd
+                    WHERE fc.nrofactura = fd.nrofactura
+                    AND fc.cliente = c.nrocli))
+
+-- NOT IN / NOT EXISTS indispensable para restas, JOINS de tablas, GROUP BY y HAVING hacer el gropy by y del resultado hacer un having || ALGO ASOCIADO A LO DIAGRAMAS, ALGO ASOCIADO A ALGEBRA RELACIONAL Y ALGO RELACIONADO A SQL
+
+Armar Modelo Relacional 
